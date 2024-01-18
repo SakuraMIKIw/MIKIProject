ba6c35837efb951ddf850b939376068b4aa6b547
MINSUNG LEE <flowerlms7@naver.com>
2024-01-18 22:18:23 +0900

    first commit

---
+++ src/project/MessgeBox.java
@@ -0,0 +1,10 @@
+package project;
+
+import static javax.swing.JOptionPane.*;
+public class MessgeBox {
+
+    public static void main(String[] args) {
+
+        showMessageDialog(null, "자바공부시작");
+    }
+}
